a068ff609288b61f86b37b02f06755f84c5aaca7
Guillaume Nault <g.nault@alphalink.fr>
2013-02-06 17:29:11 +0100

    utils: Add function for strict str to int conversion
    Define the u_readlong() function as a wrapper for strtol(). It performs
    stricter checks on the input value and let the caller specify an interval
    of acceptable values.
    
    Signed-off-by: Guillaume Nault <g.nault@alphalink.fr>

--- accel-pppd/utils.c
+++ accel-pppd/utils.c
@@ -1,4 +1,6 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "triton.h"
 #include "utils.h"
@@ -8,4 +10,23 @@
 void __export u_inet_ntoa(in_addr_t addr, char *str)
 {
 	sprintf(str, "%i.%i.%i.%i", addr & 0xff, (addr >> 8) & 0xff, (addr >> 16) & 0xff, (addr >> 24) & 0xff);
+}
+
+int __export u_readlong(long int *dst, const char *src,
+                        long int min, long int max)
+{
+        char *src_stop = NULL;
+        long int rv;
+
+        if (dst == NULL || src == NULL || src[0] == '\0')
+                return -1;
+
+        errno = 0;
+        rv = strtol(src, &src_stop, 0);
+        if (errno != 0 || *src_stop != '\0' || rv < min || rv > max) {
+                return -1;
+        } else {
+                *dst = rv;
+                return 0;
+        }
 }
--- accel-pppd/utils.h
+++ accel-pppd/utils.h
@@ -4,5 +4,6 @@
 #include <netinet/in.h>
 
 void u_inet_ntoa(in_addr_t, char *str);
+int u_readlong(long int *dst, const char *src, long int min, long int max);
 
 #endif
