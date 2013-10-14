9265a505207b8eaa59202628ac93e4a45d155d62
Dmitry Kozlov <xeb@mail.ru>
2013-10-14 13:47:57 +0400

    ippool: fixed sigsegv due to missing check for NULL (intoduced by 67ac2ba78573644d6649921bafc41ba098061e0a)

--- accel-pppd/extra/ippool.c
+++ accel-pppd/extra/ippool.c
@@ -285,10 +285,12 @@ static struct ipv4db_item_t *get_ip(struct ap_session *ses)
 		it = NULL;
 	spin_unlock(&p->lock);
 
-	if (ses->ctrl->ppp)
-		it->it.addr = conf_gw_ip_address;
-	else
-		it->it.addr = 0;
+	if (it) {
+		if (ses->ctrl->ppp)
+			it->it.addr = conf_gw_ip_address;
+		else
+			it->it.addr = 0;
+	}
 
 	return it ? &it->it : NULL;
 }
