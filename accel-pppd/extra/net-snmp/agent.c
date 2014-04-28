f9ef39d9e4e1b18bf21697598f543d7e5b288297
Guillaume Nault <g.nault@alphalink.fr>
2014-04-28 21:15:51 +0200

    Revert "net-snmp: run snmp in "special" thread"
    This reverts commit f2774e5a02d29de71eb64a64565b23090b6f6c5e.
    
    snmp_thread() needs to be run as regular thread to allow Valgrind
    analysis. Using a different namespace for file desciptors will be
    done later on using unshare().
    
    Signed-off-by: Guillaume Nault <g.nault@alphalink.fr>

--- accel-pppd/extra/net-snmp/agent.c
+++ accel-pppd/extra/net-snmp/agent.c
@@ -1,6 +1,5 @@
 #include <pthread.h>
 #include <signal.h>
-#include <sys/wait.h>
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
@@ -26,8 +25,8 @@ static int conf_master = 0;
 static oid* oid_prefix;
 static size_t oid_prefix_size;*/
 
+static pthread_t snmp_thr;
 static int snmp_term = 0;
-static int snmp_pid;
 
 /*int accel_ppp_alloc_oid(oid tail, size_t size, oid **oid)
 {
@@ -70,7 +69,7 @@ static int agent_log(int major, int minor, void *serv_arg, void *cl_arg)
 	return 0;
 }
 
-static int snmp_thread(void *a)
+static void *snmp_thread(void *a)
 {
 	sigset_t set;
 
@@ -106,22 +105,22 @@ static int snmp_thread(void *a)
 	if (conf_master)
 		init_master_agent();
 	
-	while (!snmp_term)
+	while (!snmp_term) {
     agent_check_and_process(1);
+	}
 	
 	snmp_shutdown(conf_agent_name);
 
   SOCK_CLEANUP;
 
-	return 0;
+	return NULL;
 }
 
 static void snmp_ctx_close(struct triton_context_t *ctx)
 {
-	int status;
 	snmp_term = 1;
-	kill(snmp_pid, 32);
-	waitpid(snmp_pid, &status, 0);
+	pthread_cancel(snmp_thr);
+	pthread_join(snmp_thr, NULL);
 	triton_context_unregister(ctx);
 }
 
@@ -145,8 +144,7 @@ static void init(void)
 	if (opt)
 		conf_oid_prefix = opt;*/
 	
-	snmp_pid = clone(snmp_thread, malloc(1024*1024) + 1024*1024, CLONE_SIGHAND|CLONE_FS|CLONE_VM|CLONE_THREAD, NULL);
-	
+	pthread_create(&snmp_thr, NULL, snmp_thread, NULL);
 	triton_context_register(&ctx, NULL);
 	triton_context_wakeup(&ctx);
 	triton_collect_cpu_usage();
