50f2b51b68e0fc90cc17c6e0ec3184a2759c8c6b
liguojiang <guojiang@591wifi.com>
2016-01-04 00:47:07 +0800

    Update radius.c
    Framed-IP-Address BUG
    
    radius server first, ip pool next.

--- accel-pppd/radius/radius.c
+++ accel-pppd/radius/radius.c
@@ -703,4 +703,4 @@ static void radius_init(void)
 	triton_event_register_handler(EV_CONFIG_RELOAD, (triton_event_func)load_config);
 }
 
-DEFINE_INIT(51, radius_init);
+DEFINE_INIT(52, radius_init);
