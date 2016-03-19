xebd/accel-ppp

commit d392ccd024f9243bf5df8e242a5b0cb6a85876ba

ppp: fix bug introduced by "call ap_session_finished before actual interface destroing"

accel-pppd/ppp/ppp.c
@@ -236,6 +236,12 @@ static void destablish_ppp(struct ppp_t *ppp)
 {
 	struct pppunit_cache *uc = NULL;
 
+	if (ppp->unit_fd < 0) {
+		ap_session_finished(&ppp->ses);
+		destroy_ppp_channel(ppp);
+		return;
+	}
+
 	if (conf_unit_cache) {
 		struct ifreq ifr;
 
@@ -250,11 +256,6 @@ static void destablish_ppp(struct ppp_t *ppp)
 		}
 	}
 
-	ap_session_finished(&ppp->ses);
-
-	if (ppp->unit_fd < 0)
-		goto destroy_channel;
-
 	if (conf_unit_cache) {
 		triton_md_unregister_handler(&ppp->unit_hnd, 0);
 
@@ -267,10 +268,9 @@ static void destablish_ppp(struct ppp_t *ppp)
 skip:
 	ppp->unit_fd = -1;
 
-	log_ppp_debug("ppp destablished\n");
-
-destroy_channel:
 	destroy_ppp_channel(ppp);
+
+	log_ppp_debug("ppp destablished\n");
 
 	if (uc) {
 		pthread_mutex_lock(&uc_lock);

commit 6ba85c46a6564f94a3f6bf81acf6685a991776d3

ppp: fix bug introduced by d392ccd024f9243bf5df8e242a5b0cb6a85876ba

accel-pppd/ppp/ppp.c
@@ -256,6 +256,8 @@ static void destablish_ppp(struct ppp_t *ppp)
 		}
 	}
 
+	ap_session_finished(&ppp->ses);
+
 	if (conf_unit_cache) {
 		triton_md_unregister_handler(&ppp->unit_hnd, 0);
 