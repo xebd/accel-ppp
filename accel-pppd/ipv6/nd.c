66f63b92f2f3c110384863b0539e10019b4e9838
Vladislav Grishenko <themiron@mail.ru>
2016-05-12 22:20:42 +0500

    ipv6: nd: add non-/64 prefixes support
     non-/64 subnets still needs Router Advertimenets for the
     default route & RDNSS.

--- accel-pppd/ipv6/nd.c
+++ accel-pppd/ipv6/nd.c
@@ -129,23 +129,30 @@ static void ipv6_nd_send_ra(struct ipv6_nd_handler_t *h, struct sockaddr_in6 *ad
 
 	pinfo = (struct nd_opt_prefix_info *)(adv + 1);
 	list_for_each_entry(a, &h->ses->ipv6->addr_list, entry) {
-		memset(pinfo, 0, sizeof(*pinfo));
-		pinfo->nd_opt_pi_type = ND_OPT_PREFIX_INFORMATION;
-		pinfo->nd_opt_pi_len = 4;
-		pinfo->nd_opt_pi_prefix_len = a->prefix_len;
-		pinfo->nd_opt_pi_flags_reserved =
-			((a->flag_onlink || conf_AdvPrefixOnLinkFlag) ? ND_OPT_PI_FLAG_ONLINK : 0) |
-			((a->flag_auto || (conf_AdvPrefixAutonomousFlag && a->prefix_len == 64)) ? ND_OPT_PI_FLAG_AUTO : 0);
-		pinfo->nd_opt_pi_valid_time = htonl(conf_AdvPrefixValidLifetime);
-		pinfo->nd_opt_pi_preferred_time = htonl(conf_AdvPrefixPreferredLifetime);
-		memcpy(&pinfo->nd_opt_pi_prefix, &a->addr, 8);
-		pinfo++;
+		if (a->prefix_len < 128) {
+			memset(pinfo, 0, sizeof(*pinfo));
+			pinfo->nd_opt_pi_type = ND_OPT_PREFIX_INFORMATION;
+			pinfo->nd_opt_pi_len = 4;
+			pinfo->nd_opt_pi_prefix_len = a->prefix_len;
+			pinfo->nd_opt_pi_flags_reserved =
+				((a->flag_onlink || conf_AdvPrefixOnLinkFlag) ? ND_OPT_PI_FLAG_ONLINK : 0) |
+				((a->flag_auto || (conf_AdvPrefixAutonomousFlag && a->prefix_len == 64)) ? ND_OPT_PI_FLAG_AUTO : 0);
+			pinfo->nd_opt_pi_valid_time = htonl(conf_AdvPrefixValidLifetime);
+			pinfo->nd_opt_pi_preferred_time = htonl(conf_AdvPrefixPreferredLifetime);
+			memcpy(&pinfo->nd_opt_pi_prefix, &a->addr, (a->prefix_len + 7) / 8);
+			pinfo->nd_opt_pi_prefix.s6_addr[a->prefix_len / 8] &= ~(0xff >> (a->prefix_len % 8));
+			pinfo++;
+		}
 
 		if (!a->installed) {
-			struct in6_addr addr;
-			memcpy(addr.s6_addr, &a->addr, 8);
-			memcpy(addr.s6_addr + 8, &h->ses->ipv6->intf_id, 8);
-			ip6addr_add(h->ses->ifindex, &addr, a->prefix_len);
+			if (a->prefix_len > 64)
+				ip6route_add(h->ses->ifindex, &a->addr, a->prefix_len, 0);
+			else {
+				struct in6_addr addr;
+				memcpy(addr.s6_addr, &a->addr, 8);
+				memcpy(addr.s6_addr + 8, &h->ses->ipv6->intf_id, 8);
+				ip6addr_add(h->ses->ifindex, &addr, a->prefix_len);
+			}
 			a->installed = 1;
 		}
 	}
@@ -370,7 +377,7 @@ static void ev_ses_started(struct ap_session *ses)
 		return;
 
 	list_for_each_entry(a, &ses->ipv6->addr_list, entry) {
-		if (a->prefix_len == 64) {
+		if (a->prefix_len) {
 			ipv6_nd_start(ses);
 			break;
 		}
