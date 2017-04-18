9ea88bac7d5bf8fc5cf2d5f7d0a734ec7a9e6df6
Dmitry Kozlov <xeb@mail.ru>
2017-04-18 16:07:35 +0300

    implemented session count limiting
    new config option:
    [common]
    max-sessions=N
    
    If set then accel-pppd stops reply to new connection requests if total number of sessions (active and starting) is reached specified limit.

--- accel-pppd/include/ap_session.h
+++ accel-pppd/include/ap_session.h
@@ -130,6 +130,7 @@ extern int sock_fd;
 extern int sock6_fd;
 extern int urandom_fd;
 extern struct ap_session_stat ap_session_stat;
+extern int conf_max_sessions;
 
 void ap_session_init(struct ap_session *ses);
 void ap_session_set_ifindex(struct ap_session *ses);
