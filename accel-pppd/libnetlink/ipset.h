42a50d419f709b93fbd124f8ba6e0041009e4f62
Guillaume Nault <g.nault@alphalink.fr>
2018-10-24 16:36:03 +0200

    libnetlink: make ipset.h self-contained
    We need to include <netinet/in.h> to define 'in_addr_t'.
    
    Signed-off-by: Guillaume Nault <g.nault@alphalink.fr>

--- accel-pppd/libnetlink/ipset.h
+++ accel-pppd/libnetlink/ipset.h
@@ -1,6 +1,8 @@
 #ifndef __IPSET_H
 #define __IPSET_H
 
+#include <netinet/in.h>
+
 int ipset_add(const char *name, in_addr_t addr);
 int ipset_del(const char *name, in_addr_t addr);
 int ipset_flush(const char *name);
