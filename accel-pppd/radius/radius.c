c1e20522e08f4300f329ad43c0a2f48eda7a82a7
Ben Hardill <b.hardill@gmail.com>
2020-03-09 21:25:21 +0000

    Bit better debug

--- accel-pppd/radius/radius.c
+++ accel-pppd/radius/radius.c
@@ -277,6 +277,7 @@ int rad_proc_attrs(struct rad_req_t *req)
 	struct in6_addr *b;
 	int res = 0;
 	struct radius_pd_t *rpd = req->rpd;
+	char str[INET6_ADDRSTRLEN];
 
 	req->rpd->acct_interim_interval = conf_acct_interim_interval;
 	req->rpd->acct_interim_jitter = conf_acct_interim_jitter;
@@ -391,6 +392,8 @@ int rad_proc_attrs(struct rad_req_t *req)
 			  *b = attr->val.ipv6addr;
 				dns.ses = rpd->ses;
 				dns.ip6dns1 = b;
+				inet_ntop(AF_INET6, dns.ip6dns1, str, INET6_ADDRSTRLEN);
+				log_ppp_info2("IPv6 DNS address %s\n", str);
 				break;
 		}
 	}
