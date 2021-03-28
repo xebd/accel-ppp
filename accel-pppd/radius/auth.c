xebd/accel-ppp

commit 62f7740033f05053a581e864742575a46ccc6da2

Fixed some errors found by valgrind and pvs-studio (#11)
* Fix errors found by valgrind

==12312== Syscall param sendmsg(msg.msg_iov[0]) points to uninitialised byte(s)
==12312==    at 0x486CCF0: sendmsg (in /lib/libpthread-2.22.so)
==12312==    by 0x12F57F: rtnl_talk (libnetlink.c:316)
==12312==    by 0x132DA3: genl_resolve_mcg (genl.c:52)
==12312==    by 0x484E1CB: init (vlan_mon.c:528)
==12312==    by 0x484CDC0: vlan_mon_register_proto (vlan_mon.c:48)
==12312==    by 0x510B763: load_vlan_mon (pppoe.c:1914)
==12312==    by 0x510BFF2: load_config (pppoe.c:2064)
==12312==    by 0x510C22A: pppoe_init (pppoe.c:2108)
==12312==    by 0x483E9EB: triton_load_modules (triton.c:704)
==12312==    by 0x1384B2: main (main.c:339)
==12312==  Address 0xbedacdd8 is on thread 1's stack
==12312==  in frame #2, created by genl_resolve_mcg (genl.c:23)

==12312== 15 bytes in 1 blocks are definitely lost in loss record 352 of 836
==12312==    at 0x482A9A9: malloc (in /usr/lib/valgrind/vgpreload_memcheck-x86-linux.so)
==12312==    by 0x4B97524: strdup (in /lib/libc-2.22.so)
==12312==    by 0x12C30C: init (telnet.c:769)
==12312==    by 0x483E9EB: triton_load_modules (triton.c:704)
==12312==    by 0x1384B2: main (main.c:339)
==12312==
==12312== 15 bytes in 1 blocks are definitely lost in loss record 353 of 836
==12312==    at 0x482A9A9: malloc (in /usr/lib/valgrind/vgpreload_memcheck-x86-linux.so)
==12312==    by 0x4B97524: strdup (in /lib/libc-2.22.so)
==12312==    by 0x12D60A: init (tcp.c:392)
==12312==    by 0x483E9EB: triton_load_modules (triton.c:704)
==12312==    by 0x1384B2: main (main.c:339)

* Fix another warnings by cppcheck

[accel-pppd/ctrl/ipoe/arp.c:256]: (error) Uninitialized variable: n
[accel-pppd/ctrl/pppoe/mac_filter.c:90]: (warning) %x in format string (no. 1) requires 'unsigned int *' but the argument type is 'signed int *'.

accel-pppd/radius/auth.c
@@ -147,9 +147,9 @@ static void rad_auth_finalize(struct radius_pd_t *rpd, int r)
 {
 	hold_pd(rpd);
 
-	rpd->auth_ctx->cb(rpd->auth_ctx->cb_arg, r);
-
 	if (rpd->auth_ctx) {
+		rpd->auth_ctx->cb(rpd->auth_ctx->cb_arg, r);
+
 		if (r == PWDB_SUCCESS) {
 			rpd->auth_reply = rpd->auth_ctx->req->reply;
 			rpd->auth_ctx->req->reply = NULL;

accel-pppd/auth/auth_chap_md5.c
@@ -73,7 +73,7 @@ struct chap_auth_data {
 	int failure;
 	char *name;
 	char *mschap_error;
-	int started:1;
+	unsigned int started:1;
 };
 
 static void chap_send_challenge(struct chap_auth_data *ad, int new);

accel-pppd/auth/auth_mschap_v1.c
@@ -72,7 +72,7 @@ struct chap_auth_data {
 	int failure;
 	char *name;
 	char *mschap_error;
-	int started:1;
+	unsigned int started:1;
 };
 
 static void chap_send_challenge(struct chap_auth_data *ad, int new);

accel-pppd/auth/auth_mschap_v2.c
@@ -75,7 +75,7 @@ struct chap_auth_data {
 	char *mschap_error;
 	char *reply_msg;
 	int failure;
-	int started:1;
+	unsigned int started:1;
 };
 
 static void chap_send_challenge(struct chap_auth_data *ad, int new);

accel-pppd/auth/auth_pap.c
@@ -42,7 +42,7 @@ struct pap_auth_data {
 	struct triton_timer_t timeout;
 	char *peer_id;
 	int req_id;
-	int started:1;
+	unsigned int started:1;
 };
 
 struct pap_hdr {

accel-pppd/backup/backup.h
@@ -40,7 +40,7 @@ struct backup_data
 	struct ap_session *ses;
 	struct backup_storage *storage;
 	struct list_head mod_list;
-	int internal:1;
+	unsigned int internal:1;
 };
 
 struct backup_module

accel-pppd/cli/tcp.c
@@ -30,8 +30,8 @@ struct tcp_client_t {
 	uint8_t *cmdline;
 	int xmit_pos;
 	int recv_pos;
-	int auth:1;
-	int disconnect:1;
+	unsigned int auth:1;
+	unsigned int disconnect:1;
 };
 
 struct buffer_t {
@@ -357,10 +357,6 @@ static void start_server(const char *host, int port)
     return;
 	}
 
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(port);
-	addr.sin_addr.s_addr = inet_addr(host);
-
 	triton_context_register(&serv_ctx, NULL);
 	triton_context_set_priority(&serv_ctx, 0);
 	triton_md_register_handler(&serv_ctx, &serv_hnd);
@@ -407,6 +403,7 @@ static void init(void)
 
 	triton_event_register_handler(EV_CONFIG_RELOAD, (triton_event_func)load_config);
 
+	free(host);
 	return;
 err_fmt:
 	log_emerg("cli: tcp: invalid format\n");

accel-pppd/cli/telnet.c
@@ -47,11 +47,11 @@ struct telnet_client_t {
 	int cmdline_pos;
 	int cmdline_pos2;
 	int cmdline_len;
-	int auth:1;
-	int echo:1;
-	int telcmd:1;
-	int esc:1;
-	int disconnect:1;
+	unsigned int auth:1;
+	unsigned int echo:1;
+	unsigned int telcmd:1;
+	unsigned int esc:1;
+	unsigned int disconnect:1;
 };
 
 struct buffer_t {
@@ -694,10 +694,6 @@ static void start_server(const char *host, int port)
     return;
 	}
 
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(port);
-	addr.sin_addr.s_addr = inet_addr(host);
-
 	triton_context_register(&serv_ctx, NULL);
 	triton_context_set_priority(&serv_ctx, 0);
 	triton_md_register_handler(&serv_ctx, &serv_hnd);
@@ -793,6 +789,7 @@ static void init(void)
 
 	triton_event_register_handler(EV_CONFIG_RELOAD, (triton_event_func)load_config);
 
+	free(host);
 	return;
 err_fmt:
 	log_emerg("cli: telnet: invalid format\n");

commit a77f9e071f084918bbb805c2509aec0f47fcf95b

radius: fix segfault regression after 62f7740033f05053a581e864742575a46ccc6da2

accel-pppd/radius/auth.c
@@ -147,9 +147,10 @@ static void rad_auth_finalize(struct radius_pd_t *rpd, int r)
 {
 	hold_pd(rpd);
 
-	if (rpd->auth_ctx) {
+	if (rpd->auth_ctx)
 		rpd->auth_ctx->cb(rpd->auth_ctx->cb_arg, r);
 
+	if (rpd->auth_ctx) {
 		if (r == PWDB_SUCCESS) {
 			rpd->auth_reply = rpd->auth_ctx->req->reply;
 			rpd->auth_ctx->req->reply = NULL;